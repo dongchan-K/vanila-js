432c79c640831f11dff17422ea462ffade3e6c54
dongchan-K <ehdcks1028@gmail.com>
2021-08-13 18:41:24 +0900

    feat: 무한 스크롤 기본 기능 구현

--- InfiniteScroll/app.js
+++ InfiniteScroll/app.js
@@ -1,4 +1,53 @@
-window.onload = async () => {
-  const res = await fetch('https://picsum.photos/v2/list?page=1&limit=5');
-  const items = await res.json();
+const $wrapper = document.querySelector('.wrapper');
+const $target = document.querySelector('.items');
+const $loaderWrapper = document.querySelector('.loader-wrapper');
+const $notice = document.querySelector('.notice');
+const $movingBtn = document.querySelector('.moving-btn');
+
+let options = {
+  root: null,
+  threshold: 1.0,
+};
+let target = $target.lastElementChild;
+
+let count = document.querySelectorAll('.items > li').length;
+
+const callback = (entries, observer) => {
+  if (!entries[0].isIntersecting) return;
+
+  if (count === 20) {
+    $loaderWrapper.style.display = 'none';
+    $notice.style.display = 'flex';
+    return;
+  }
+
+  $loaderWrapper.style.display = 'flex';
+  setTimeout(() => {
+    const $fragment = document.createDocumentFragment();
+
+    for (let i = 0; i < 5; i++) {
+      const $li = document.createElement('li');
+      $li.classList.add('item');
+      $fragment.appendChild($li);
+    }
+    $target.appendChild($fragment);
+
+    count = document.querySelectorAll('.items > li').length;
+    target = $target.lastElementChild;
+    observer.disconnect();
+    observer.observe(target);
+    $loaderWrapper.style.display = 'none';
+  }, 1000);
+};
+
+let observer = new IntersectionObserver(callback, options);
+
+observer.observe(target);
+
+$movingBtn.onclick = () => {
+  $wrapper.scroll({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
 };
